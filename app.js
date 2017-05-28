67d79fb481a1b9713cc799f267a1065e967093a8
omolewa <akinyomiomolewa30gmail.com>
2017-05-28 05:14:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var config = {apiKey: "AIzaSyDEh8bDoRC7JSYk9pd8353nxr2qKTy3AN0",
+    authDomain: "omolewa-39d61.firebaseapp.com",
+    databaseURL: "https://omolewa-39d61.firebaseio.com",
+    projectId: "omolewa-39d61",
+    storageBucket: "omolewa-39d61.appspot.com",
+    messagingSenderId: "795832675103"
+};
+firebase.initialieApp(config)
+var myFirebase = firebase.database().ref();
+
+var submitrecommendationForm = function function_name () {
+	var name = $("#name").val();
+	var sex = $("#sex").val();
+	var phone number = $("#phonenumber#").val;
+	// body...
+};
+recommendations.push({
+	"name" = "the name of the person"
+	"sex" = "the sexof the person"
+	"phone number" = "the phone number of the individual"
+});
+
+$window.load(function() {
+	$("#recommendationForm").submit(submitRecommendation)
+	// body...
+});
